44395d510e6db6711712f029dd722c068aaf4913
KapilLanjewar <37495732+KapilLanjewar@users.noreply.github.com>
2023-09-14 23:53:19 +0530

    Add files via upload
    Eight puzzle problem solving

---
+++ matrix/eight_puzzle_iterative_deepening_search/iddfs.py
@@ -0,0 +1,41 @@
+import itertools
+
+from solver import Solver
+
+
+class IDDFS(Solver):
+    def __init__(self, initial_state, steps):
+        super(IDDFS, self).__init__(initial_state)
+        self.frontier = []
+        self.steps = steps
+
+    def dls(self, limit):
+        self.frontier.append(self.initial_state)
+        while self.frontier:
+            board = self.frontier.pop()
+            self.explored_nodes.add(tuple(board.state))
+            if board.goal_test():
+                self.set_solution(board)
+                return self.solution
+            if board.depth < limit:
+                for neighbor in board.neighbors()[::-1]:
+                    if tuple(neighbor.state) not in self.explored_nodes:
+                        self.frontier.append(neighbor)
+                        self.explored_nodes.add(tuple(neighbor.state))
+                        self.max_depth = max(self.max_depth, neighbor.depth)
+        return None
+
+    def solve(self):
+        for i in itertools.count():
+            self.frontier = []
+            self.explored_nodes = set()
+            self.max_depth = 0
+            self.frontier.append(self.initial_state)
+            sol = self.dls(i)
+            self.steps = i
+            if sol is not None:
+                break
+        return
+
+    def number_of_steps(self):
+        return self.steps
